60b2d42e0c0f14bbb20c250a1672c909dcdf9c67
zfh1997 <zfh1997@mail.ustc.edu.cn>
2018-08-25 00:21:58 -0400

    Add files via upload

--- src/core_model_modal.py
+++ src/core_model_modal.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 1-D model
-Module Name :Core
+Module Name : Core
 Only Diffusion included
 A PDE Solver
 
@@ -18,12 +18,13 @@ import numpy as np
 import random as rand 
 
 def pde_solver(time_step, level, c, v, w, TTL = 25, \
-                Dilution_type = 'None', H_Dissipate = False, V_Sedimentation = False, V_Transportation = False):
+                Dilution_type = 'Force', H_Dissipate = False, V_Sedimentation = False, V_Transportation = False, Diffusion_Coef = "Huang"):
     """
     This is a pde_solver for solving diffusion equation
 
     use Crank-Nicolson method
     webpage: georg.io/2013/12/Crank_Nicolson
+             en.wikipedia.org/wiki/Crank%E2%80%93Nicolson_method
 
      C_t = D * C_zz - w * C_z + f(z)
           ||
@@ -49,77 +50,82 @@ def pde_solver(time_step, level, c, v, w, TTL = 25, \
     assert (time_step > 0 and level > 0), "Initial level should be positive"
     exceed_flag = 0
 
-    # sigma_c = D * delta_t / 2 * (delta_x)^2
-    # delta_t = 1 month delta_x = 500 meter
-    # True Value : sigma_c = 2.5e-4
     modal_number = 3
-    delta_z = 500 #units : meter
+    delta_z = 1 #units : meter
     delta_t = 1   #units : month
-    delta_y = 2
+    delta_y = 1
+
+    #w = w * 86400
+    v = v * 0.5
+    w = w * 10
 
     #Concentration in Reservoir
     c_exchange = np.zeros((modal_number, time_step, level))
 
-    #Vertical Eddy Diffusion Coefficient K_zz
-    #Huang(1979) parameterization method
-    #b : coefficient
-    #n : power of the formula
-    b = 1e-3
-    n = 2.5
-    #D_zz = b * (level * 500)**n
-    D_zz = np.full((modal_number), 1e5)
+    if Diffusion_Coef == "Huang":
+        #Vertical Eddy Diffusion Coefficient D_zz
+        #Huang(1979) parameterization method
+        #b : coefficient
+        #n : power of the formula
+        #D_zz = b * (level * 500)**n
+        b = 1e-1
+        n = 0.5
+        D_zz_generator = lambda z : b * (z**n)
+        level_height = np.linspace(0,80,80)
+        D_zz = D_zz_generator(level_height)
+        for modal in range(modal_number):
+            D_zz = np.vstack((D_zz, D_zz_generator(level_height)))
 
-    # Unit transist from m/s to m/month
-    dis_rate = np.full((modal_number), 1)
+    if Diffusion_Coef == "Constant":
+        print("Constant")
+        D_zz = np.full((modal_number, level), 6)
 
-    #w = w * 86400 * 30 / delta_z
-    #v = v * 86400 * 30
-    w = w * 0
-    #v = v * 0
-    
     #Concatenate to expand wind into many years
     for i in range(int(time_step / 12)):
         v = np.concatenate((v, v), axis = 0)
     for i in range(int(time_step / 12)):
         w = np.concatenate((w, w), axis = 0)
-        w = np.absolute(w)
 
     #Different Modal
     for modal in range(modal_number):
         #~~~~~~~Solve Part
         for time in range(0, time_step - 1):
-            print(time)
+            cross_term = (w[time, i] * delta_z * delta_t)/2 
+        # Init A ------>  time + 1
+            A =     np.diagflat([-1 * D_zz[modal, i] * delta_t - cross_term for i in range(level - 1)], -1)
+            A = A + np.diagflat([-1 * D_zz[modal, i] * delta_t + cross_term for i in range(level - 1)], 1)
+            A = A + np.diagflat([D_zz[modal, 0] * delta_t + 2 * (delta_z)**2 - cross_term] \
+            + [2 * D_zz[modal, i] * delta_t + 2 * (delta_z)**2 for i in range(level - 2)] \
+            + [D_zz[modal, level - 1] * delta_t + 2 * (delta_z)**2 + cross_term], 0)
 
-        # Init A ------> time + 1
-            A = np.diagflat([-1 * delta_z * delta_t * w[time, i] - D_zz[modal] * delta_t  for i in range(level - 1)], -1)
-            A = A + np.diagflat([delta_z * delta_t * w[time, i + 1] - D_zz[modal] * delta_t for i in range(level - 1)], 1)
-            A = A + np.diagflat(\
-            [-2 * delta_z * delta_t * w[time, 0] + D_zz[modal] * delta_t - 2 * (delta_z)**2]\
-            + [-2 * D_zz[modal] * delta_t - 2 * (delta_z)**2 for i in range(level - 2)]\
-            + [D_zz[modal] * delta_t - 2 * (delta_z)**2 + delta_z * delta_t * w[time, level]], 0)
+        # Init B ------> time
+            B =     np.diagflat([D_zz[modal, i] * delta_t + cross_term for i in range(level - 1)], -1)
+            B = B + np.diagflat([D_zz[modal, i] * delta_t - cross_term for i in range(level - 1)], 1)
+            B = B + np.diagflat([-1 * D_zz[modal, 0] * delta_t + 2 * (delta_z)**2 + cross_term] \
+            + [-2 * D_zz[modal, i] * delta_t + 2 * (delta_z)**2 for i in range(level - 2)] \
+            + [-1 * D_zz[modal, level - 1] * delta_t + 2 * (delta_z)**2 - cross_term], 0)
 
-        # Init B ------>  time
-            B = np.diagflat([D_zz[modal] * delta_t for i in range(level - 1)], -1)
-            B = B + np.diagflat([D_zz[modal] * delta_t  for i in range(level - 1)], 1)
-            B = B + np.diagflat(\
-            [-1 * D_zz[modal] * delta_t - 2 * (delta_z)**2] \
-            + [-2 * D_zz[modal] * delta_t - 2 * (delta_z)**2 for i in range(level - 2)] \
-            + [-2 * (delta_z)**2], 0)
-
-            #u for dissipate process
-            #Solve use np linalg module
+            #U for dissipate process
+            #Exchange Aerosol with a zero-initial reservoirs
+            #Solve use numpy linalg module
             delta_c = c[modal, time, :] - c_exchange[modal, time, :]
-            #delta_y = 7e2
-            if any(delta_c < 0):
-                c[modal, time + 1, :] = np.linalg.solve(A, B.dot(c[modal, time, :]))
-            else:
-                c[modal, time + 1, :] = np.linalg.solve(A, \
-                B.dot(c[modal, time, :]) - 2 * delta_t * (delta_z)**2 * v[time, :] * delta_c / delta_y)
+            if all(delta_c >= 0):
+                c_new = np.linalg.solve(A, B.dot(c[modal, time, :]) - 2 * delta_t * (delta_z)**2 * v[time, :] * delta_c / delta_y)
+                if any(c_new < 0) :
+                    c[modal, time + 1, :] = 0 * c[modal, time + 1, :]
+                else :
+                    c[modal, time + 1, :] = c_new
                 c_exchange[modal, time + 1, :] = (c_exchange[modal, time, :] + delta_c * v[time, :] / delta_y)
+            else:
+                c[modal, time + 1, :] = np.linalg.solve(A, B.dot(c[modal, time, :]))
+
+            del A, B
 
         #~~~~~~~Physical Process Part
             # Horizontal dissipation
             if H_Dissipate == True:
+                # Unit transist from m/s to m/month
+                dis_rate = np.full((modal_number), 1)
                 for i in range(level):
                     if c[modal, time + 1, i] > 0:
                         h_flux =  v[time + 1, i] * dis_rate[modal]
@@ -150,6 +156,8 @@ def pde_solver(time_step, level, c, v, w, TTL = 25, \
             if Dilution_type == 'Force':
                 # top of troposphere = 15000m ~ 30 lev
                 c[modal, time + 1, 0:TTL] = 0
+                if time == 0:
+                    c[modal, time, 0:TTL] = 0
             # Stochastic Force Dilution
             if Dilution_type == 'Stochastic':
                 rand.seed(time)
@@ -162,17 +170,20 @@ def pde_solver(time_step, level, c, v, w, TTL = 25, \
             # None Dilution
             if Dilution_type == 'None':
                 c[modal, time + 1, :] = c[modal, time + 1, :] * 1.0
+
             #Restriction : To prevent the infinite diverge of c
-            exceed_cond = any(c[modal, time, :] > level * 4 * modal) or \
-            any(c[modal, time, :] < 0)
+            #exceed_cond = any(c[modal, time, :] > level * 100 * modal) or \
+            #any(c[modal, time, :] < 0)
+            exceed_cond = any(c[modal, time, :] < 0)
             if exceed_cond:
                 exceed_flag = 1
                 break
+
         #Check to avoid the singularity
         if exceed_flag == 1:
-            c[modal, time + 1, :] = 0 * c[modal, time + 1, :]
-            c[modal, time, :] = 0 * c[modal, time, :]
-            print("Warning : Data has exceed")
+            #c[modal, time, :] = 0 * c[modal, time, :]
+            print("Warning : Data has exceed until " + str(time))
+            break
 
            
     c_total = np.sum(c, axis = 0)
